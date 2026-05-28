3485c9219ece098397838b9d5576b08ca6a24d70
Hadassa <166148227+Hadassa-gomes@users.noreply.github.com>
2026-05-28 19:36:08 -0300

    Create aluno.py

---
+++ aluno.py
@@ -0,0 +1,28 @@
+# aluno.py
+
+class Aluno:
+def **init**(self, nome):
+self.nome = nome
+self.notas = []
+
+```
+def adicionar_nota(self, nota):
+    if nota < 0 or nota > 10:
+        raise ValueError("Nota inválida")
+    self.notas.append(nota)
+
+def calcular_media(self):
+    if not self.notas:
+        return 0
+    return sum(self.notas) / len(self.notas)
+
+def verificar_situacao(self):
+    media = self.calcular_media()
+
+    if media >= 7:
+        return "Aprovado"
+    elif media >= 5:
+        return "Recuperação"
+    else:
+        return "Reprovado"
+```
